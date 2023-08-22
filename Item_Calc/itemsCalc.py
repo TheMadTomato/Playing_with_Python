3829d0cb1ce08b1d97643a8ab32549ba2ea7b2d1
TheMadTomato <TheMadTomato@proton.me>
2023-08-22 10:46:44 +0300

    More stable version

--- Item_Calc/itemsCalc.py
+++ Item_Calc/itemsCalc.py
@@ -4,11 +4,11 @@ import openpyxl
 BEER_PRICE = 2
 WINE_PRICE = 3
 BLUE_WINE_PRICE = 4
-BEER_TOKEN = 1
-WHITE_WINE_TOKEN = 3
-RED_WINE_TOKEN = 3
-PINK_WINE_TOKEN = 3
-BLUE_WINE_TOKEN = 1
+BEER_TOKEN = "beer"
+WHITE_WINE_TOKEN = "white"
+RED_WINE_TOKEN = "red"
+PINK_WINE_TOKEN = "pink"
+BLUE_WINE_TOKEN = "blue"
 
 # Initialize variables for tracking sales and revenue
 total_sales = 0
@@ -24,49 +24,46 @@ worksheet = workbook.active
 worksheet.title = "Sales Data"
 
 # Set column headers
-headers = ["Sale ID", "Beer Tokens", "White Wine Tokens", "Red Wine Tokens", "Pink Wine Tokens", "Blue Wine Tokens", "Money Paid", "Total Price"]
+headers = ["Sale ID", "Item", "Money Paid", "Total Price"]
 worksheet.append(headers)
 
 # Function for calculating the total price of an order
-def calculate_price(beer_tokens, white_wine_tokens, red_wine_tokens, pink_wine_tokens, blue_wine_tokens):
-    total_tokens = beer_tokens + white_wine_tokens + red_wine_tokens + pink_wine_tokens + blue_wine_tokens
-    total_price = (beer_tokens * BEER_PRICE) + (white_wine_tokens * WINE_PRICE) + (red_wine_tokens * WINE_PRICE) + (pink_wine_tokens * WINE_PRICE) + (blue_wine_tokens * BLUE_WINE_PRICE)
-    if total_tokens != (BEER_TOKEN + WHITE_WINE_TOKEN + RED_WINE_TOKEN + PINK_WINE_TOKEN + BLUE_WINE_TOKEN):
-        raise ValueError("Invalid token combination")
-    return total_price
-
-# Function for converting dollars to LBP
-def dollars_to_lbp(dollars):
-    return dollars * 100000
+def calculate_price(item, quantity):
+    if item == BEER_TOKEN:
+        return quantity * BEER_PRICE
+    elif item in (WHITE_WINE_TOKEN, RED_WINE_TOKEN, PINK_WINE_TOKEN):
+        return quantity * WINE_PRICE
+    elif item == BLUE_WINE_TOKEN:
+        return quantity * BLUE_WINE_PRICE
+    else:
+        raise ValueError("Invalid item")
 
 # Function for processing a sale
-def process_sale(money_paid, beer_tokens, white_wine_tokens, red_wine_tokens, pink_wine_tokens, blue_wine_tokens):
-    total_price = calculate_price(beer_tokens, white_wine_tokens, red_wine_tokens, pink_wine_tokens, blue_wine_tokens)
+def process_sale(item, quantity, money_paid):
+    total_price = calculate_price(item, quantity)
     if money_paid < total_price:
         raise ValueError("Insufficient funds")
+    
     sale_id = len(sales_data) + 1
     sales_data[sale_id] = {
-        "beer_tokens": beer_tokens,
-        "white_wine_tokens": white_wine_tokens,
-        "red_wine_tokens": red_wine_tokens,
-        "pink_wine_tokens": pink_wine_tokens,
-        "blue_wine_tokens": blue_wine_tokens,
+        "item": item,
+        "quantity": quantity,
         "money_paid": money_paid,
         "total_price": total_price
     }
-    global total_sales
-    global total_dollars
-    global total_lbp
+    
+    global total_sales, total_dollars, total_lbp
     total_sales += total_price
     total_dollars += money_paid
-    total_lbp += dollars_to_lbp(money_paid)
+    total_lbp += money_paid * 100000
 
     # Add sale data to the Excel worksheet
-    worksheet.append([sale_id, beer_tokens, white_wine_tokens, red_wine_tokens, pink_wine_tokens, blue_wine_tokens, money_paid, total_price])
+    worksheet.append([sale_id, item, money_paid, total_price])
 
 # Example usage
-process_sale(6, 1, 0, 0, 0, 1) # Buy 1 beer and 1 blue wine for $6
-process_sale(15, 0, 3, 0, 0, 0) # Buy 1 white wine, 1 red wine, and 1 pink wine for $15
+process_sale(BEER_TOKEN, 1, 3)  # Buy 1 beer for $3
+process_sale(BLUE_WINE_TOKEN, 1, 4)  # Buy 1 blue wine for $4
+process_sale(RED_WINE_TOKEN, 2, 6)  # Buy 2 red wines for $6
 
 # Save the workbook to a file
 workbook.save("sales_data.xlsx")
@@ -75,11 +72,8 @@ workbook.save("sales_data.xlsx")
 print("Sales Data:")
 for sale_id, sale_data in sales_data.items():
     print(f"Sale #{sale_id}:")
-    print(f"  Beer tokens: {sale_data['beer_tokens']}")
-    print(f"  White wine tokens: {sale_data['white_wine_tokens']}")
-    print(f"  Red wine tokens: {sale_data['red_wine_tokens']}")
-    print(f"  Pink wine tokens: {sale_data['pink_wine_tokens']}")
-    print(f"  Blue wine tokens: {sale_data['blue_wine_tokens']}")
+    print(f"  Item: {sale_data['item']}")
+    print(f"  Quantity: {sale_data['quantity']}")
     print(f"  Money paid: ${sale_data['money_paid']}")
     print(f"  Total price: ${sale_data['total_price']}")
 
