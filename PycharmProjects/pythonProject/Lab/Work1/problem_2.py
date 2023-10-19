145aeef4d8315f6376feded56bbf6bf5d60643af
TheMadTomato <TheMadTomato@proton.me>
2023-10-19 21:35:24 +0300

    updating format related matter

--- PycharmProjects/pythonProject/Lab/Work1/problem_2.py
+++ PycharmProjects/pythonProject/Lab/Work1/problem_2.py
@@ -1,12 +1,11 @@
 import time
 
 print("Epoch to GMT Conversion")
-epoch_time = time.time()
+#epoch_time = time.time() #To test it with the second amount from January 1, 1970 till now.
+epoch_time = 86399 # The amount of seconds in a single day
 
-# Dividing epoch_time by 31536000 to get the year
-year = epoch_time // 31536000
 # Dividing epoch_time by 86400 to get the days
-days = (epoch_time % 31536000) // 86400
+days = epoch_time // 86400
 # Dividing epoch_time by 3600 to get the hours
 hours = (epoch_time % 86400) // 3600
 # Dividing the remainder of the above division by 60 to get the minutes
