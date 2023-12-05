3986f657cbe714ba830fad8ae79395ec4a81fa46
TheMadTomato <TheMadTomato@proton.me>
2023-12-05 21:52:04 +0200

    start workin

---
+++ PyNet/DataFrame_emulation.py
@@ -0,0 +1,9 @@
+"""
+ PDU at Layer 2 are called frames. the Datalink have 2 sublayers: LLC and MAC
+ LLC: Logical Link Control which involves the logical part of the link such as flow control, error checking,
+      and frame synchronization.
+ MAC: Media Access Control which involves the physical part of the link such as the topology,
+      the transmission technique, and the physical addressing.
+
+ The DataFrame is a class that emulate the DataFrame of the Datalink Layer
+"""
