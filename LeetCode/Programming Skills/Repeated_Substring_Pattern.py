d1d17cc384408d27cdb259ce1669d7286582c106
TheMadTomato <TheMadTomato@proton.me>
2023-12-12 20:26:12 +0200

    started playing with leetcode

--- LeetCode/Programming Skills/Repeated_Substring_Pattern.py
+++ LeetCode/Programming Skills/Repeated_Substring_Pattern.py
@@ -24,4 +24,31 @@ Constraints:
 1 <= s.length <= 104
 s consists of lowercase English letters.
 """
+# Attempt 1: missread the question if followed the key word in question i would have solved it
+# class Solution:
+#     def repeatedSubstringPattern(self, s: str) -> bool:
+#         list_a = []
+#         list_b = []
+#         for i in range(len(s)):
+#             if s[i] not in list_a:
+#                 list_a.append(s[i])
+#                 print("list_a:", list_a)
+#             elif s[i] in list_a:
+#                 list_b.append(s[i])
+#                 print("list_b:", list_b)
+#             if len(list_b) == len(list_a):
+#                 break
+#         print("list_a:", list_a)
+#         print("list_b:", list_b)
+#         if list_a == list_b:
+#             return True
+#         else:
+#             return False
 
+# method 2: got the help from the web, it is a nw syntax for me so i got to learn a new thing today >3
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return s in (s + s)[1:-1]
+# test
+s = "ababba"
+print(Solution().repeatedSubstringPattern(s))
