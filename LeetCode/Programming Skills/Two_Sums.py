0e2f777465ca3adb346efa457e2f702a26a56427
TheMadTomato <TheMadTomato@proton.me>
2023-12-22 21:45:10 +0200

    adding exercise of the day

---
+++ LeetCode/Programming Skills/Two_Sums.py
@@ -0,0 +1,19 @@
+from typing import List
+class Solution(object):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i,j]
+
+
+# test
+nums = [3,3]
+target = 6
+sol = Solution()
+print(sol.twoSum(nums,target))
